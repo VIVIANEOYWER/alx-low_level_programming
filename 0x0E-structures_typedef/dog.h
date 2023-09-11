429c0305e68eb2b828fcfda0a0c61865dadf6a98
VIVIANEOYWER <veeoywer@gmail.com>
2023-09-11 06:04:44 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdlib.h>
+#include "dog.h"
+
 /**
- * struct dog - Dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Desc: header file
+ * init_dog - intializes a variable of type struct dog
+ * @d: pointer to struct dog to initalize
+ * @name: name to initialize
+ * @age age to initialize
+ * @owner: Owner to initialize
+ * Desc: Header File
  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
