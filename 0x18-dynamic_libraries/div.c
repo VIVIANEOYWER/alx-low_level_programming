7f5b1a4512cf192869f5167348cb1bbcef7ec4e4
VIVIANEOYWER <vivianeoywer@gmail.com>
2023-12-15 06:54:07 -0800

    More commits

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * div - divide  two integers
+ * @a: first int
+ * @b: second int
+ * Return final answer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - mode of two integers
+ * @a: first int
+ * @b: second int
+ * Return:final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
